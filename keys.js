a7b69cc44039a985376dd693c55a08cfba432eed
Sara Javed <sarajaved@Saras-MacBook-Air.local>
2017-10-23 23:00:19 -0400

    packages working

---
+++ keys.js
@@ -0,0 +1,11 @@
+
+```JavaScript
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'ZMnsXPxTg8tNtdPl4VuFiIOBd',
+  consumer_secret: 'CkbVQJSgWmALXKQpfhYXx7Rx9nzueb80MVPA9ziMw0VZKZBKht',
+  access_token_key: '3224868128-mpX8b55oudIp6GiMfxDaptN7lFziMY69HcIdnWW',
+  access_token_secret: 'epWoBaMscc3hVrjLMaUQljdFbO1ccfAMrUHyAJo2YS5kn',
+}
+```
